1ccd2eb03aeb1462f9663a2855e01f9c288ae311
sidkos <sergey@pheno.ai>
2025-10-03 15:30:51 +0300

    ci unittests fix 2

--- src/metadata_manager/migrations/0001_initial.py
+++ src/metadata_manager/migrations/0001_initial.py
@@ -1,6 +1,6 @@
 from django.db import migrations, models
 
-import src.metadata_manager.models.user
+import metadata_manager.models.user
 
 
 class Migration(migrations.Migration):
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
                         primary_key=True,
                         serialize=False,
                         unique=True,
-                        validators=[src.metadata_manager.models.user.User.validate_israeli_id],
+                        validators=[metadata_manager.models.user.User.validate_israeli_id],
                     ),
                 ),
                 ("name", models.CharField(help_text="Full name (required).", max_length=100)),
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                     models.CharField(
                         help_text="Phone number in E.164 format (e.g., +972...).",
                         max_length=20,
-                        validators=[src.metadata_manager.models.user.User.validate_phone],
+                        validators=[metadata_manager.models.user.User.validate_phone],
                     ),
                 ),
                 ("address", models.CharField(help_text="Street address (required).", max_length=255)),
